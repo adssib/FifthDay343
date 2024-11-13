38b51b276e3da68ba7ded5f35e2d67262a0c3690
safaa <157904919+sknt11@users.noreply.github.com>
2024-11-13 15:04:17 -0500

    Backend to link contact/payment info to delivery request object

--- Server/server.js
+++ Server/server.js
@@ -3,21 +3,22 @@ const express = require('express');
 const db = require('./config/db'); // Importing the singleton database connection
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const deliveryRoutes = require('./routes/deliveryRoutes'); 
+const deliveryRoutes = require('./routes/deliveryRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Logging middleware
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
-  });
+});
 
 // Test Database Connection Route
 app.get('/test-connection', async (req, res) => {
@@ -31,7 +32,8 @@ app.get('/test-connection', async (req, res) => {
 });
 
 // API routes
-app.use('/api', deliveryRoutes); 
+app.use('/api', deliveryRoutes);
+app.use('/api/payment', paymentRoutes);
 
 // Start the server
 app.listen(PORT, () => {
